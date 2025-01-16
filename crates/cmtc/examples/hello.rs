63980e83cc7598335a18c922f721f538da48f946
Youwei Xiao <shallwe@pku.edu.cn>
2025-01-16 19:13:15 +0800

    update hello example

--- crates/cmtc/examples/hello.rs
+++ crates/cmtc/examples/hello.rs
@@ -1,59 +1,62 @@
-use cmtc::ehdl::*;
-use cmtrs::*;
-use cmtrs::stl::*;
+// use cmtc::ehdl::*;
+// use cmtrs::*;
+// use cmtrs::stl::*;
 
-itfc_declare! {
-  param T;
-  struct A2B {
-    a: input param T,
-    b: output param T,
-  }
-  method run(a) -> (b);
-}
+// itfc_declare! {
+//   param T;
+//   struct A2B {
+//     a: input param T,
+//     b: output param T,
+//   }
+//   method run(a) -> (b);
+// }
 
-#[module]
-fn add1() -> A2B {
-  let t = Type::UInt(4);
-  let io = io! { T: &t};
-  let run = method!(
-    (io.a) -> (io.b) {
-      ret!(io.a + 1.lit(&t))
-    }
-  );
-}
+// #[module]
+// fn add1() -> A2B {
+//   let t = Type::UInt(4);
+//   let io = io! { T: &t};
+//   let run = method!(
+//     (io.a) -> (io.b) {
+//       ret!(io.a + 1.lit(&t))
+//     }
+//   );
+// }
 
 
-itfc_declare! {
-  struct Tb {}
-}
+// itfc_declare! {
+//   struct Tb {}
+// }
 
-#[module]
-fn mk_tb() -> Tb {
-  io!{};
-  anno!("is_tb": "true");
-  let add1 = instance!(add1());
-  let mut cycle = instance!(Integer::new());
-  named_always! {format!("input");
-    [cycle.lt(int(10))]
-    (){
-      let i = int_as(&cycle, &Type::UInt(4));
-      let b = add1.run(i);
-      sim_print!("cycle ", cycle, ": output=", b);
-      cycle %= &cycle + int(1);
-    }
-  };
+// #[module]
+// fn mk_tb() -> Tb {
+//   io!{};
+//   anno!("is_tb": "true");
+//   let add1 = instance!(add1());
+//   let mut cycle = instance!(Integer::new());
+//   named_always! {format!("input");
+//     [cycle.lt(int(10))]
+//     (){
+//       let i = int_as(&cycle, &Type::UInt(4));
+//       let b = add1.run(i);
+//       sim_print!("cycle ", cycle, ": output=", b);
+//       cycle %= &cycle + int(1);
+//     }
+//   };
 
-  named_always! {format!("exit");
-    [cycle.eq(int(10))]
-    (){
-      sim_print!("exit");
-      sim_exit!();
-    }
-  };
-}
+//   named_always! {format!("exit");
+//     [cycle.eq(int(10))]
+//     (){
+//       sim_print!("exit");
+//       sim_exit!();
+//     }
+//   };
+// }
 
-fn main() -> anyhow::Result<()> {
-  elaborate(add1(), sv_config("add1.sv"))?;
-  elaborate(mk_tb(), ksim_config("./tb/add1"))?;
-  Ok(())
+// fn main() -> anyhow::Result<()> {
+//   elaborate(add1(), sv_config("add1.sv"))?;
+//   Ok(())
+// }
+
+fn main() {
+  println!("Hello, world!");
 }
